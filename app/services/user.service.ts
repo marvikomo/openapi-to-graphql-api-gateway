3238e096dfbabd1d65c4b1f24129ae81696be246
erin deji <erin.deji@gmail.com>
2024-06-18 10:49:29 +0100

    validate schema types

--- app/services/user.service.ts
+++ app/services/user.service.ts
@@ -13,13 +13,19 @@ export class UserService {
     }
   };
 
-  public createMe = async (
-    currentUser
-  ) => {
+  public createMe = async (currentUser) => {
     try {
       if (!currentUser) throw new GraphQLError("not data returned!");
       console.log(currentUser);
       return Helpers.success(currentUser);
+    } catch (error) {
+      return Helpers.error(error?.message);
+    }
+  };
+
+  public createOrder = async (createOrderData) => {
+    try {
+      return Helpers.success(createOrderData);
     } catch (error) {
       return Helpers.error(error?.message);
     }
